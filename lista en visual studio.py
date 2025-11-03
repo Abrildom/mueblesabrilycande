2d7900f5336f64cc399e77f8cac9c035774817d4
Malenitt <garigliomalena@gmail.com>
2025-11-03 14:00:38 -0300

    l

---
+++ lista en visual studio.py
@@ -0,0 +1,8 @@
+lista=[ ]
+for i in range(10):
+          numero=input ("Ingrese un numero ")
+          lista.append(numero)
+lista.sort ()
+print(lista)
+suma=suma(lista)
+print ("La suma de los numeros ingresados es:" , suma)
